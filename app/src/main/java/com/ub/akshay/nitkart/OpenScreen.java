88906f01b7c86ae2dd41398257dd7cbeb6f18e9c
Akshay Upadhya B <akshayupadhya@gmail.com>
2018-03-17 11:27:06 +0530

    Final version for seller login, handling event listeners and removing buggy checkbox method

--- app/src/main/java/com/ub/akshay/nitkart/OpenScreen.java
+++ app/src/main/java/com/ub/akshay/nitkart/OpenScreen.java
@@ -1,13 +1,12 @@
 package com.ub.akshay.nitkart;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -18,20 +17,20 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class OpenScreen extends AppCompatActivity {
     private static final String TAG = OpenScreen.class.getSimpleName();
+    ProgressBar progressBar;
     private Button loginButton;
     private EditText user, pass;
     private TextView newUser, resetPassword, newSeller;
     private String username, password;
-    CheckBox checkBox;
-
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-
-    ProgressBar progressBar;
-    Boolean isSeller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,23 +45,36 @@ public class OpenScreen extends AppCompatActivity {
         resetPassword = (TextView) findViewById(R.id.forgotPassword);
         progressBar = (ProgressBar) findViewById(R.id.loginPageProgressBar);
 
-        checkBox = (CheckBox) findViewById(R.id.isseller);
-
-
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     // User is signed in
                     Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                    Toast.makeText(getApplicationContext(), "Sign in Successful!", Toast.LENGTH_SHORT).show();
-                    progressBar.setVisibility(View.GONE);
-                    Intent intent = new Intent(getApplicationContext(), MainAppPage.class);
-                    intent.putExtra("isuserseller", isSeller);
-                    startActivity(intent);
-                    finish();
+                    FirebaseDatabase.getInstance().getReference("sellers/" + user.getUid())
+                            .addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    Intent intent = new Intent(getApplicationContext(), MainAppPage.class);
+                                    if (dataSnapshot.exists()) {
+                                        intent.putExtra("isuserseller", true);
+                                    } else {
+                                        intent.putExtra("isuserseller", false);
+                                    }
+                                    Toast.makeText(getApplicationContext(), "Sign in Successful!", Toast.LENGTH_SHORT).show();
+                                    progressBar.setVisibility(View.GONE);
+                                    startActivity(intent);
+                                    finish();
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+                                    // Failed to read value
+                                    Log.w(TAG, "Failed to read value.", databaseError.toException());
+                                }
+                            });
                 } else {
                     // User is signed out
                     Log.d(TAG, "onAuthStateChanged:signed_out");
@@ -77,7 +89,6 @@ public class OpenScreen extends AppCompatActivity {
                 progressBar.setVisibility(View.VISIBLE);
                 username = user.getText().toString();
                 password = pass.getText().toString();
-                isSeller = checkBox.isChecked();
                 setInputs(false);
                 signIn(username, password);
             }
@@ -125,7 +136,7 @@ public class OpenScreen extends AppCompatActivity {
         }
     }
 
-    public void signIn(String email, String password){
+    public void signIn(String email, String password) {
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
